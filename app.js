794df701d958ca0598defe54304e1dbb780685a7
stevearchuleta <stevearchuleta@me.com>
2021-04-01 11:38:57 -0700

    add package.json, package-lock.json, and .gitignore

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
+const schema = require('./schema/schema');
 
 const app = express();
 
 //===============
 // MIDDLEWARE: When a user goes to this endpoint (/graphql), 
-// the require('express-graphql') function is called to interact with the endpoint, such that gaphQL requests are handled.
+// the require('express-graphql') function is called to interact with the endpoint, such that graphQL requests are handled.
 // The graphqlHTTP function takes a schema (in order to work properly)
 //===============
 app.use('/graphql', graphqlHTTP({
-
+    schema: schema
 }));
 
 app.listen(5000, () => {
